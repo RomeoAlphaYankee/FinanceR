39bc1924f77cdf35b93faa09931da6de3fb02eca
Ray <44300713+RomeoAlphaYankee@users.noreply.github.com>
2019-08-07 22:49:33 -0400

    Add files via upload

--- AptsPairsTrade.R
+++ AptsPairsTrade.R
@@ -26,7 +26,7 @@ pairs(as.zoo(apts))
 cor(apts)
 cov(apts)
 
-plot.zoo(apts[ , 1:4])
+plot.zoo(apts[ , 1:7])
 
 # Strongest correlation between EQR and AVB
 apts$ratio <- apts$AVB / apts$EQR
@@ -34,19 +34,28 @@ plot(apts$ratio)
 sd(apts$ratio)
 mean(apts$ratio)
 
-apts$sma <- rollapply(apts$ratio, mean, width = 20)
-apts$sma252 <- rollapply(apts$ratio, mean, width = 252 * 5)
+
+# Establish some technical indicators
+apts$sma <- rollapply(apts$ratio, mean, width = 252 * 5)
 apts$sd <- rollapply(apts$ratio, sd, width = 252)
+
+# Plot some indicators
+par(mfrow = c(2, 1))
+plot(apts$ratio)
 
 plot(apts$ratio["2010/"], main = "AVB / EQR")
 lines(mean(apts$ratio["2010/"]) + 2 * apts$sd, col = "red")
 lines(mean(apts$ratio["2010/"]) - 2 * apts$sd, col = "red")
-lines(apts["2010/", "sma252"], col = "blue")
+lines(apts["2010/", "sma"], col = "blue")
 lines(apts$sma252 + 2 * apts$sd, col = "green")
 lines(apts$sma252 - 2 * apts$sd, col = "green")
 
+Rel.strength <- data.frame(RSI(apts$ratio)["2010/"])
+plot(x = as.Date(rownames(Rel.strength)), y = Rel.strength$rsi, type = "l")
+abline(h = c(30, 70), lty = 2, col = "red")
+
 # Second strongest correlation is between UDR and AIV
-apts$ratio2 <- apts[ , 4] / apts[ , 3]
+apts$ratio2 <- apts$AIV / apts$UDR
 apts$sma2 <- rollapply(apts$ratio2, mean, width = 252 * 5)
 apts$sd2 <- rollapply(apts$ratio2, sd, width = 252)
 
@@ -57,14 +66,31 @@ lines(apts["2010/", "sma2"], col = "blue")
 lines(apts$sma2 + 2 * apts$sd2, col = "green")
 lines(apts$sma2 - 2 * apts$sd2, col = "green")
 
+Rel.strength <- data.frame(RSI(apts$ratio)["2010/"])
+plot(x = as.Date(rownames(Rel.strength)), y = Rel.strength$rsi, type = "l")
+abline(h = c(30, 70), lty = 2, col = "red")
+
 # Third strongest correlation is between MAA and CPT
-apts$ratio3 <- apts[ , 6] / apts[ , 5]
+apts$ratio3 <- apts$MAA / apts$CPT
 apts$sma3 <- rollapply(apts$ratio3, mean, width = 252 * 5)
 apts$sd3 <- rollapply(apts$ratio3, sd, width = 252)
 
-plot(apts$ratio3["2010/"], main = "CPT / MAA")
+plot(apts$ratio3["2010/"], main = "MAA / CPT")
 lines(mean(apts$ratio3["2010/"]) + 2 * apts$sd3, col = "red")
 lines(mean(apts$ratio3["2010/"]) - 2 * apts$sd3, col = "red")
 lines(apts["2010/", "sma3"], col = "blue")
 lines(apts$sma3 + 2 * apts$sd3, col = "green")
 lines(apts$sma3 - 2 * apts$sd3, col = "green")
+
+# Fourth strongest correlation is between ESS and UDR
+apts$ratio4 <- apts$ESS / apts$UDR
+apts$sma4 <- rollapply(apts$ratio4, mean, width = 252 * 5)
+apts$sd4 <- rollapply(apts$ratio4, sd, width = 252)
+
+plot(apts$ratio4["2010/"], main = "ESS / UDR")
+lines(mean(apts$ratio4["2010/"]) + 2 * apts$sd4, col = "red")
+lines(mean(apts$ratio4["2010/"]) - 2 * apts$sd4["2010/"], col = "red")
+
+lines(apts["2010/", "sma4"], col = "blue")
+lines(apts$sma4 + 2 * apts$sd4, col = "green")
+lines(apts$sma4 - 2 * apts$sd4, col = "green")
